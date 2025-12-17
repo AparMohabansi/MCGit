ee851fcec41dcdef90d4c6dcb5aa28eecc66fe86
Apar <apar.mohabansi@gmail.com>
2025-12-16 19:20:00 -0500

    Initial commit

---
+++ src/client/java/com/mcgit/MCGitClient.java
@@ -0,0 +1,10 @@
+package com.mcgit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCGitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/mcgit/MCGitDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mcgit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MCGitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
